9a4d3fc9f2bb8c2aa04751c09fc4564721857fdc
jeremylshepherd <jeremylshepherd@gmail.com>
2016-04-10 00:00:23 -0400

    Updated server db connection.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 var routes = require('./routes/index');
 
 /*
-mongoose.connect('mongodb://localhost/urlapp', function(err, db) {
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds019028.mlab.com:19028/heroku_pdw06x16', function(err, db) {
   if(err) {console.log(err);}
 
   console.log('Connected to urlapp');
