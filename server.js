15d72d488cbcad9809cfbb93879b15f70fac94ce
jeremylshepherd <jeremylshepherd@gmail.com>
2016-04-10 20:39:56 -0400

    CORS test code.

--- server.js
+++ server.js
@@ -44,6 +44,20 @@ if (app.get('env') === 'development') {
   });
 }
 
+//TEST - Allow Cross Domain POST
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'POST,OPTIONS');
+   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Cache-Control");
+   if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    return res.end();
+  } else {
+    return next();
+  }
+});
+
+
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
