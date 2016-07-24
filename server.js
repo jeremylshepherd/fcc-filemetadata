83255ecd872fcb51946a84ecf9b392c66ea32dfe
Jeremy Shepherd <jeremylshepherd@gmail.com>
2016-07-24 01:05:26 +0000

    Troubleshooting Heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
