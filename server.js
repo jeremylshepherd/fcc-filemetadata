8b3a4337880b5e61143c194583c9b6ff1f25cec5
Jeremy Shepherd <jeremylshepherd@gmail.com>
2016-07-24 02:00:16 +0000

    Added body-parser to resolve problem with Heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
 require('dotenv').load();
@@ -8,6 +9,9 @@ var routes = require('./routes/index');
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
 app.enable('trust proxy');
@@ -22,15 +26,15 @@ app.use(function(req, res, next) {
 
 // development error handler
 // // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 
 // production error handler
